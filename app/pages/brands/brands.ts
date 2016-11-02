237afc06ff161a3742545d626ac06c5234f3b397
kolexinfos <kolexinfos@gmail.com>
2016-11-02 14:44:50 +0000

    Implemented Brand Pages with provider

--- app/pages/brands/brands.ts
+++ app/pages/brands/brands.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+
+import { UserProvider } from '../../providers/user-provider/user-provider';
+import { BrandProvider } from '../../providers/brand-provider/brand-provider';
 
 /*
   Generated class for the BrandsPage page.
@@ -9,13 +12,47 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   templateUrl: 'build/pages/brands/brands.html',
+  providers : [BrandProvider, UserProvider]
 })
 export class BrandsPage {
   
   search: {text?:string,email?:string} = {};
+  brands = [];
 
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController,
+  private brandProvider: BrandProvider, 
+  private userProvider:UserProvider,
+  private loadingCtrl: LoadingController
+  ) 
+  {
+    this.GetBrands();
+  }
+  
+  GetBrands(){
+    let loadingPopup = this.loadingCtrl.create({
+      content: 'Loading data...'
+    });
+    
+    loadingPopup.present();
 
+    this.brandProvider.GetBrands().subscribe(
+        data => {
+          console.log(data.result);
+          this.brands = data.result;
+          loadingPopup.dismiss();
+
+         //_.filter(data.result, {likes: [{email: this.user.email}] });
+        },
+        err => {
+          loadingPopup.dismiss();
+          console.log(err);
+          
+      },
+      () => {
+        console.log('Pulling data');
+        loadingPopup.dismiss();
+      }
+    )
   }
 
 }
